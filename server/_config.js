bdfce8484d3ee35ee6f644a388660474f30bb52b
amerriman <amm434@gmail.com>
2015-09-22 13:59:03 -0600

    updated mongo connections

--- server/_config.js
+++ server/_config.js
@@ -2,7 +2,8 @@ var config = {};
 
 config.mongoURI = {
   development: "mongodb://localhost/node-testing",
-  test: "mongodb://localhost/node-test"
+  test: "mongodb://localhost/node-test",
+  stage: "mongodb://heroku_wp860jdn:f5jhkobaramp61u7om4ggjk14l@ds051883.mongolab.com:51883/heroku_wp860jdn"
 };
 
 module.exports = config;
