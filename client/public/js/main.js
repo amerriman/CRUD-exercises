d6f7b3ecedc67eaa838ea18e56f0673ae6f288e9
amerriman <amm434@gmail.com>
2015-09-21 19:15:30 -0600

    delete and edit buttons added but not functional

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -31,7 +31,8 @@ function listExercises(){
   $.get('/api/exercises', function(data){
     console.log(data);
     for (var i = 0; i < data.length; i++) {
-      $('#exercise-list').append("<tr><td>"+ data[i].name+"</td><td>"+data[i].description+"</td><td>"+data[i].tags+"</td></tr>");
+      $('#exercise-list').append("<tr>"+"<td>"+data[i].name+"</td><td>"+data[i].description+"</td><td>"+data[i].tags+"</td>" + '<td><a class="btn btn-danger btn-xs delete-button" id="'+data[i]._id+'" role="button">Delete</a>'+ '&nbsp;<a class="btn btn-primary btn-xs edit-button" id="'+data[i]._id+'" role="button">Edit</a></td>'+
+        "</tr>");
     }
   });
 }
