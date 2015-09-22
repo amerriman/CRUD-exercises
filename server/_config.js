7a76d35a95ed2457f0be8cf9f558c2993d5c9f6b
amerriman <amm434@gmail.com>
2015-09-22 14:09:20 -0600

    heroku tweaks

--- server/_config.js
+++ server/_config.js
@@ -3,7 +3,7 @@ var config = {};
 config.mongoURI = {
   development: "mongodb://localhost/node-testing",
   test: "mongodb://localhost/node-test",
-  stage: "mongodb://heroku_wp860jdn:f5jhkobaramp61u7om4ggjk14l@ds051883.mongolab.com:51883/heroku_wp860jdn"
+  production: "mongodb://heroku_wp860jdn:f5jhkobaramp61u7om4ggjk14l@ds051883.mongolab.com:51883/heroku_wp860jdn"
 };
 
 module.exports = config;
